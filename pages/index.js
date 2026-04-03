f01c2824052966decea55d350e2cf22d319aa721
Kaio da Costa Silva <kaio@jmjsistemas.com.br>
2026-04-02 23:31:23 -0400

    refactor: update homepage greeting message with a more heartfelt tone

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te amo amoi. Vou resolver nossos problemas e vou precisar da sua ajuda! Feliz páscoa. (:</h1>
+    return <h1>Juntos vamos superar tudo — só preciso de você ao meu lado. Te amo amooi❤️ Feliz Páscoa! 🐣</h1>
 }
 
 export default Home;
