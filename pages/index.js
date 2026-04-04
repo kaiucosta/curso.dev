46dc14f0cb225e8977799b8684386c758eb08cb6
Kaio da Costa Silva <kaio@jmjsistemas.com.br>
2026-04-03 22:51:26 -0400

    Corrige mensage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Juntos vamos superar tudo — só preciso de você ao meu lado. Te amo amooi❤️ Feliz Páscoa! 🐣</h1>
+    return <h1>Juntos vamos superar tudo — só preciso de você ao meu lado. Te amo amooi❤️ Se você acredita que vamos conseguir, me dar um beijo e um abraço bem apertado.</h1>
 }
 
 export default Home;
